e7cf4fcd89770eb16f2c7a23f4492d0c3afe7fbf
Kenneth Thomas <Ken@Kenneths-MacBook-Pro.local>
2016-07-18 12:35:30 -0400

    formatted controller

--- app/controllers/winning_word_lengths_controller.rb
+++ app/controllers/winning_word_lengths_controller.rb
@@ -1,15 +1,16 @@
 class WinningWordLengthsController < ApplicationController
   def index
 
-    @winning_word_length = WinningWordLength.all
-    @venomous_spider_deaths = VenomousSpiderDeath.all
-    @data = "Data!"
+    winning_word_length = WinningWordLength.all
+    venomous_spider_deaths = VenomousSpiderDeath.all
+    year = winning_word_length.map { |word| word.year }
+    word_length = winning_word_length.map{ |word| word.length }
+    spider_deaths = venomous_spider_deaths.map{ |year| year.death_count }
+
+    @data = [year, word_length, spider_deaths].transpose
     respond_to do |format|
       format.html
       format.json do
-        year = @winning_word_length.map { |word| word.year }
-        word_length = @winning_word_length.map{ |word| word.length }
-        spider_deaths = @venomous_spider_deaths.map{ |year| year.death_count }
         render json: {year: year, wordLength: word_length, spiderDeaths: spider_deaths}
       end
     end
